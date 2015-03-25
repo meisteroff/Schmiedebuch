13cde14ad87b8be6878346b6b2557b4d390fce1d
GuN <GuN>
2015-03-25 10:10:25 +0100

    organize for more clarity

--- src/particles/Particle.java
+++ src/particles/Particle.java
@@ -10,12 +10,13 @@ public class Particle {
 	float positionX, positionY;
 	float velocityX, velocityY;
 	float accelerationX, accelerationY;
+	
+	float rotation;
 	float angularVelocity;
 	float angularAcceleration;
 
 	float size;
 	float red, green, blue, alpha;
-	float rotation;
 
 	int age, lifetime;
 	
